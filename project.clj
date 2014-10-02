09b9d7993b608f6093a9d8cf17bc15fde46775da
Michael Drogalis <mjd3089@rit.edu>
2014-10-01 17:02:49 -0700

    0.3.3 release.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject onyx-plugin/lein-template "0.3.2"
+(defproject onyx-plugin/lein-template "0.3.3"
   :description "A Leiningen 2.0 template for new Onyx plugins"
   :url "http://github.com/MichaelDrogalis/onyx-plugin"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies  [[com.mdrogalis/onyx "0.3.2"]]
+  :dependencies  [[com.mdrogalis/onyx "0.3.3"]]
   :eval-in-leiningen true)
--- resources/leiningen/new/onyx_plugin/README.md
+++ resources/leiningen/new/onyx_plugin/README.md
@@ -7,7 +7,7 @@ Onyx plugin for {{medium}}.
 In your project file:
 
 ```clojure
-[onyx-{{medium}} "0.3.2"]
+[onyx-{{medium}} "0.3.3"]
 ```
 
 In your peer boot-up namespace:
