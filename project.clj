ed06798666003a875ed46bc2e10c860454b075a2
Lucas Bradstreet <lucas.bradstreet@onyxplatform.org>
2015-08-06 03:03:13 +0200

    Add lein-set-version to fix CI

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies  [[org.onyxplatform/onyx "0.7.0"]]
+  :plugins [[lein-set-version "0.4.1"]]
   :eval-in-leiningen true)
