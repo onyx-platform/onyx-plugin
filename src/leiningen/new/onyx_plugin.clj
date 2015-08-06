20679fd7f1eae99339370fb15bc56710273e365f
Lucas Bradstreet <lucas.bradstreet@onyxplatform.org>
2015-08-06 03:19:05 +0200

    Sanitize medium name in file names

--- src/leiningen/new/onyx_plugin.clj
+++ src/leiningen/new/onyx_plugin.clj
@@ -10,12 +10,13 @@
   (let [data {:name name
               :medium medium
               :clojure-version "1.6.0"
-              :sanitized (name-to-path name)}]
+              :sanitized (name-to-path name)}
+        sanitized-medium (name-to-path medium)]
     (main/info "Generating fresh 'lein new' onyx-plugin project.")
     (->files data
              ["README.md" (render "README.md" data)]
              ["project.clj" (render "project.clj" data)]
-             ["src/onyx/plugin/{{medium}}_input.clj" (render "medium_input.clj" data)]
-             ["test/onyx/plugin/{{medium}}_input_test.clj" (render "medium_input_test.clj" data)]
-             ["src/onyx/plugin/{{medium}}_output.clj" (render "medium_output.clj" data)]
-             ["test/onyx/plugin/{{medium}}_output_test.clj" (render "medium_output_test.clj" data)])))
+             [(str "src/onyx/plugin/" sanitized-medium "_input.clj") (render "medium_input.clj" data)]
+             [(str "test/onyx/plugin/" sanitized-medium "_input_test.clj") (render "medium_input_test.clj" data)]
+             [(str "src/onyx/plugin/" sanitized-medium "_output.clj") (render "medium_output.clj" data)]
+             [(str "test/onyx/plugin/" sanitized-medium "_output_test.clj") (render "medium_output_test.clj" data)])))
