7cda845f99ab2a39fc0174e9c2ea11f91f214e21
Michael Drogalis <mjd3089@rit.edu>
2015-11-11 19:00:15 -0800

    Adjust version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onyx-plugin/lein-template "0.8.0.0-alpha1"
+(defproject onyx-plugin/lein-template "0.8.0.0"
   :description "A Leiningen 2.0 template for new Onyx plugins"
   :url "http://github.com/MichaelDrogalis/onyx-plugin"
   :license {:name "Eclipse Public License"
