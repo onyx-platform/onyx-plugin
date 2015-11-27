838f50e801a91aa93c138d7cc331d7f6cfa31be6
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-11-27 16:55:07 +0800

    Fix README versioning

--- scripts/release.sh
+++ scripts/release.sh
@@ -33,7 +33,6 @@ then
 elif [[ "$new_version" == *[.]*[.]* ]]; 
 then
 	core_version=$new_version
-	#lein update-dependency org.onyxplatform/onyx $core_version
 	if [[ "$version_type" == "$new_version" ]]; then 
 		new_plugin_version=$version_base".0"
 	else
