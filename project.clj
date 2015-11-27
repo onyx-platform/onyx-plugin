f55e0cb6fddf09d7433d4e257aeea27ac1f784f1
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-11-27 16:10:45 +0800

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onyx-plugin/lein-template "0.8.1.2"
+(defproject onyx-plugin/lein-template "0.8.1.3-SNAPSHOT"
   :description "A Leiningen 2.0 template for new Onyx plugins"
   :url "http://github.com/MichaelDrogalis/onyx-plugin"
   :license {:name "Eclipse Public License"
